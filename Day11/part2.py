eec7b96079ca418eeb6016a5f5dde9d5a3e598bb
Konrad Krzeminski <konradkrzeminski@MacBook-Pro-Konrad-2.local>
2022-12-25 19:39:02 +0100

    part1 and part2 day11 done

---
+++ Day11/part2.py
@@ -0,0 +1,107 @@
+import copy
+import math
+
+NUMBER_ROUND = 10000
+
+
+class Monkey:
+    def __init__(self, index, list_items, operator, operation_value, test_number, if_true_index, if_false_index):
+        self.index = index
+        self.list_items = list_items
+        self.operator = operator
+        self.operation_value = operation_value
+        self.test_number = test_number
+        self.if_true_index = if_true_index
+        self.if_false_index = if_false_index
+
+
+def create_init_monkeys_array(lines):
+    parsing_lines = [line.split(" ") for line in lines]
+    monkey_array = []
+    monkey_data = []
+    for line in parsing_lines:
+        if len(line) == 1:
+            continue
+
+        elif line[0] == 'Monkey':
+            monkey_data = []
+            monkey_data.append(int(line[1][:-1]))
+
+        elif line[2] == 'Starting':
+            items_list = []
+            elements = line[4:]
+            for element in elements:
+                item = int(element[:-1]) if element[-1] == ',' else int(element)
+                items_list.append(item)
+            monkey_data.append(items_list)
+
+        elif line[2] == 'Operation:':
+            if line[7] != 'old':
+                monkey_data.append(line[6])
+                monkey_data.append(int(line[7]))
+            else:
+                monkey_data.append('**')
+                monkey_data.append(2)
+
+        elif line[2] == 'Test:':
+            monkey_data.append(int(line[5]))
+
+        elif line[4] == 'If':
+            monkey_data.append(int(line[9]))
+            if line[5] == 'false:':
+                new_monkey = Monkey(monkey_data[0], monkey_data[1], monkey_data[2], monkey_data[3],
+                                    monkey_data[4], monkey_data[5], monkey_data[6])
+                monkey_array.append(new_monkey)
+
+    return monkey_array
+
+
+def inspections_for_monkey(monkey, monkey_array, lcm):
+    list_items = copy.deepcopy(monkey.list_items)
+    monkey.list_items = []
+    for item in list_items:
+        item = item % lcm
+        if monkey.operator == '*':
+            value = (item * monkey.operation_value)
+        elif monkey.operator == '+':
+            value = (item + monkey.operation_value)
+        else:
+            value = (item * item)
+
+        if value % monkey.test_number == 0:
+            monkey_array[monkey.if_true_index].list_items.append(value)
+        else:
+            monkey_array[monkey.if_false_index].list_items.append(value)
+
+
+def find_array_numbers_lcm(array_numbers):
+    actual_lcm = math.lcm(array_numbers[0], array_numbers[1])
+    for i in range (2, len(array_numbers)):
+        actual_lcm = math.lcm(actual_lcm, array_numbers[i])
+
+    return actual_lcm
+
+
+def inspect_all_monkey_all_round(monkey_array):
+    divide_factor_array = [monkey.test_number for monkey in monkey_array]
+    lcm = find_array_numbers_lcm(divide_factor_array)
+    monkey_operation_counter = [0] * len(monkey_array)
+    n = len(monkey_array)
+
+    for i in range(NUMBER_ROUND):
+        for j in range(n):
+            monkey = monkey_array[j]
+            monkey_operation_counter[monkey.index] += len(monkey.list_items)
+            inspections_for_monkey(monkey, monkey_array, lcm)
+
+    return monkey_operation_counter
+
+
+filename = "text.txt"
+
+with open(filename) as file:
+    lines = [line.rstrip() for line in file]
+
+sorted_array = sorted(inspect_all_monkey_all_round(create_init_monkeys_array(lines)))
+print(sorted_array)
+print(sorted_array[-1]*sorted_array[-2])
